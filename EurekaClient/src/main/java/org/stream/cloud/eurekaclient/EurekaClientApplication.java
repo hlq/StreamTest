e15553fcfb8e7fd5cb150ab66f03b58156a411cb
Stream <javaeer@qq.com>
2019-10-15 17:31:48 +0800

    20191015

--- EurekaClient/src/main/java/org/stream/cloud/eurekaclient/EurekaClientApplication.java
+++ EurekaClient/src/main/java/org/stream/cloud/eurekaclient/EurekaClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,13 @@ public class EurekaClientApplication {
 
     @Value("${server.port}")
     String port;
+    
+    @Value("${from}")
+    private String from;
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "Stream") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+        return "hi " + name + " ,i am from port:" + port + " !!! " + from;
     }
     
     
