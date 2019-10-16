c9462c78a06c31f62f8484ee8c33ada8d5e41987
Stream <javaeer@qq.com>
2019-10-16 16:50:50 +0800

    20191016

--- EurekaClient/src/main/java/org/stream/cloud/eurekaclient/EurekaClientApplication.java
+++ EurekaClient/src/main/java/org/stream/cloud/eurekaclient/EurekaClientApplication.java
@@ -3,12 +3,14 @@ package org.stream.cloud.eurekaclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
--- EurekaTwoClient/src/main/java/org/stream/cloud/eurekatwoclient/EurekaTwoClientApplication.java
+++ EurekaTwoClient/src/main/java/org/stream/cloud/eurekatwoclient/EurekaTwoClientApplication.java
@@ -3,11 +3,13 @@ package org.stream.cloud.eurekatwoclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
@@ -20,9 +22,12 @@ public class EurekaTwoClientApplication {
     @Value("${server.port}")
     String port;
 
+    @Value("${to}")
+    private String to;
+
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "Stream") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+        return "hi " + name + " ,i am from port:" + port + " !!! " + to;
     }
 
 }
